01ace2a9e3fa36f9eb8237c27492755e93f33f92
hellokirana <intaniasyahla@gmail.com>
2025-01-23 11:03:50 +0700

    FIX : view - dashboard

---
+++ resources/views/components/master/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="footer-left">Copyright &copy; Pemira HMJTK POLBAN - 2024</div>
+    <div class="footer-right">Template by <a href="https://nauval.in/">Muhamad Nauval Azhar</a> & Develop by <a
+            href="https://arcakranusantara.com/">Arcakranusantara</a></div>
+</footer>
